fe35c79a8bb5d2206390cd06af42670ce5334dc9
Tyrica Terry Kapral <34420663+tyt3@users.noreply.github.com>
2023-03-30 20:00:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+"""Module for creating Flask App"""
+from flask import Flask, render_template, request
+#from views import views
+#from auth import auth
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+"""Set routes/endpoints"""
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        pass
+
+    if request.method == 'POST':
+        pass
+
+    return render_template("index.html", title=
+                           "Pitt Digital Scholarship Database")
+
+if __name__ == "__main__":
+    app.run(debug=True)
