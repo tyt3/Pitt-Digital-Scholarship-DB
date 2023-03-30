040e6511a111a34bc87aa3ed43ef85e21d8f929e
Tyrica Terry Kapral <34420663+tyt3@users.noreply.github.com>
2023-03-30 16:15:51 +0000

    Add accepted values for fields w/ controlled vocab

---
+++ templates/controlled_vocab.py
@@ -0,0 +1,93 @@
+affiliation_type = ['Doctoral',
+                    'Faculty',
+                    'Masters',
+                    'Partner - Community',
+                    'Partner - Other University',
+                    'Postdoctoral',
+                    'Staff',
+                    'Undergraduate',
+                    'Other'
+                    ]
+
+campus = ['Bradford',
+          'Greensburg',
+          'Johnstown',
+          'Pittsburgh',
+          'Titusville'
+          ]
+
+career_level = ['Undergraduate',
+                'Masters',
+                'Doctoral',
+                'Postdoctoral',
+                'Professional'
+                ]
+
+category_name = ['2D Scanning and Digitization',
+                 '3D Scanning and Modeling',
+                 'Coding and Computation',
+                 'Data',
+                 'Digital Creation',
+                 'Digital Storytelling',
+                 'Digital Publishing',
+                 'Mapping and Spatial Analysis', # GIS
+                 'Metadata',
+                 'Project Managment',
+                 'Text Mining and Analysis',
+                 'Web Development',
+                 'Other'
+                 ]
+
+entity_type = ['person',
+               'unit',
+               'department',
+               'subunit'
+               ]
+
+frequency = ['Annually',
+             'Biannually',
+             'Semesterly',
+             'Fall',
+             'Spring',
+             'Summer',
+             'Other'
+             ]
+
+funding_type = ['Fellowship',
+                'Graduate Student Assistant',
+                'Grant',
+                'Internship',
+                'Research Assistant',
+                'Scholarship',
+                'Other',
+                ]
+
+payment_type = ['Hourly',
+                'Stipend',
+                'Other',
+                ]
+
+proficiency_level = ['Fundamental Awareness (basic knowledge)',
+                     'Novice (limited experience)',
+                     'Intermediate (practical application)',
+                     'Advanced (applied theory)',
+                     'Expert (recognized authority)'
+                     ]
+
+subunit_type = ['Group',
+                'Lab',
+                'Library',
+                'Office',
+                'Team',
+                'Unit',
+                'Other'
+                ]
+
+superior = ['department',
+            'subunit'
+            ]
+
+support_type = ['Collaboratory only',
+                'Formal',
+                'Informal'
+                ]
