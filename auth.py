4e1256293113b43e046b54b385f2f2d2c45cd394
Tyrica Terry Kapral <tyt3@pitt.edu>
2023-04-03 10:52:27 -0400

    Update website pages

--- auth.py
+++ auth.py
@@ -4,19 +4,19 @@ from models import User
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    return "<h1>Login</h1>"
-
-@auth.route('/logout')
-def logout():
-    return "<h1>Logout</h1>"
-
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     # if request.method == 'GET':
     #     pass
-  
+
     # if request.method == 'POST':
     #   pass
-    return "<h1>Sign Up</h1>"
+    return render_template("sign-up.html", title="Sign Up | Pitt Digital Scholarship Database")
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html", title="Login | Pitt Digital Scholarship Database")
+
+@auth.route('/logout')
+def logout():
+    return render_template("logout.html", title="Logout | Pitt Digital Scholarship Database")
--- views.py
+++ views.py
@@ -6,15 +6,15 @@ views = Blueprint('views', __name__)
 
 """Function to Show Homepage"""
 @views.route('/')
-def home():
+def index():
     return render_template("index.html", title="Pitt Digital Scholarship Database")
 
 """Function to Show Search Page"""
 @views.route('/search')
 def search():
-    return "<h1>Search</h1>"
+    return render_template("search.html", title="Search | Pitt Digital Scholarship Database")
 
 """Function to Show Create Page"""
 @views.route('/create')
 def create():
-    return "<h1>Search</h1>"
+    return render_template("create.html", title="Create | Pitt Digital Scholarship Database")
