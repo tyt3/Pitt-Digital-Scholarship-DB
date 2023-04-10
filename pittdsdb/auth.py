38757258f415b0547d83f57f721d7a327362fd72
Navodita Mathur <navoditamathur@Navoditas-MacBook-Air.local>
2023-04-09 22:08:00 -0400

    auth account get and update

--- pittdsdb/auth.py
+++ pittdsdb/auth.py
@@ -96,4 +96,51 @@ def logout():
 
 @auth_bp.route('/account')
 def account():
-    return render_template("account.html", title="Account | Pitt Digital Scholarship Database")
+    if 'email' in session:
+        email = session['email']
+        user = User.query.filter_by(email=email).first()
+        return render_template("account.html", title="Account | Pitt Digital Scholarship Database", first_name = user.first_name, last_name=user.last_name, user_name=user.user_name, email=user.email, user_password=sha256_crypt.encrypt(user.user_password), api_key=user.api_key, permission_level=user.permission_level, account_created = user.account_created, last_login=user.lase_login)
+    else:
+        return render_template("login.html", title="Login | Pitt Digital Scholarship Database", msg = "Please login to see the account details")
+
+@auth_bp.route('/update_account')
+def update_account():
+    if 'email' in session and request.method == "POST":
+        email = session['email']
+        user = User.query.filter_by(email=email).first()
+        if user is not None:
+            email = request.form.get('email')
+            password = request.form.get('password')
+            regex = '^[a-z0-9]+@pitt.edu$'
+            if re.search(regex,email):
+                first_name = request.form.get('first_name')
+                last_name = request.form.get('last_name')
+                user_name = request.form.get('user_name')
+                flag = 0
+                if (len(password)<8):
+                    msg = "Password should be min 8 characters"
+                elif not re.search("[a-z]", password):
+                    msg = "Password should contain atleast 1 lower case alphabet"
+                elif not re.search("[A-Z]", password):
+                    msg = "Password should contain atleast 1 upper case alphabet"
+                elif not re.search("[0-9]", password):
+                    msg = "Password should contain atleast 1 number"
+                elif not re.search("[_@()*&^%#<>,$]" , password):
+                    msg = "Password should contain atleast 1 special character"
+                if len(msg) == 0:
+                    p_level = request.form.get('permission_level')
+                    if p_level > 4 or p_level < 1:
+                        msg= "Invalid Permission"
+                    else:
+                        user.first_name = first_name
+                        user.last_name = last_name
+                        user.user_name = user_name
+                        user.password = sha256_crypt.encrypt(password)
+                        user.permission_level = p_level
+                        db_session.commit()
+                        return redirect(url_for('auth_bp.account'))
+            else:
+                msg = "Please register using pitt.edu email address"
+        return render_template("account.html", title="Account | Pitt Digital Scholarship Database", first_name = first_name, last_name=last_name, user_name=user_name, email=email, user_password="", api_key=api_key, permission_level=p_level, account_created = user.account_created, last_login=user.lase_login, msg = msg)
+    else:
+        return render_template("login.html", title="Login | Pitt Digital Scholarship Database", msg = "Please login to update the account details")
