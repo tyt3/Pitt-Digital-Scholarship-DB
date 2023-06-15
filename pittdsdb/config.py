db05f1ed533d160f3e2467a484bab296998fb333
Tyrica Terry Kapral <tyt3@pitt.edu>
2023-06-15 14:46:39 -0400

    Update config.py

--- pittdsdb/config.py
+++ pittdsdb/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
-TESTING = True
-DEBUG = True
+TESTING = FALSE
+DEBUG = FALSE
 FLASK_ENV = 'development'
 SECRET_KEY = environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
